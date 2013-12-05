f3539defd442512429ba000ff1f581b0414895f4
sanjay <sanjay@sanjay-Lenovo-IdeaPad-Y410P>
2013-12-04 21:17:34 -0500

    Major changes to temporal API

--- api/protected/config/main.php
+++ api/protected/config/main.php
@@ -38,6 +38,7 @@ return array(
             'rules' => array(
                 'gii' => 'gii',
                 'stacked-area-chart' => 'Home/StackedAreaChart',
+                'get-data' => 'Threat/GetData',
                 'temporal-view' => 'Home/TemporalView',
                 'temporal-view-data' => 'Threat/TemporalGraph',
                 'gii/<controller:\w+>' => 'gii/<controller>',
@@ -53,7 +54,7 @@ return array(
         'db' => array(
             'connectionString' => 'pgsql:host=localhost;port=5432;dbname=db_crimeatlanta',
             'username' => 'postgres',
-            'password' => 'irhtayag',
+            'password' => 'infovis',
             'charset' => 'utf8',
         ),
         'errorHandler' => array(
